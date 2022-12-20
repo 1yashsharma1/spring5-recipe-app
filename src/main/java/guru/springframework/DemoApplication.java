88ce1f9c56113e1cc444e764302f8033a7f1b735
1yashsharma1 <syash832@gmail.com>
2022-12-20 20:30:50 +0530

    new commit

--- src/main/java/guru/springframework/DemoApplication.java
+++ src/main/java/guru/springframework/DemoApplication.java
@@ -3,12 +3,15 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("This is first run");
+	log.debug("Just a debug message");
+		
 	}
 
 }
